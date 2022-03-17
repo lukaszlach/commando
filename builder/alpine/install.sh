934a2704d27af4e24f02b3c7464161808fdbbf34
Jeremy Wu <hoozecn@users.noreply.github.com>
2022-03-17 18:49:29 +0800

    Update install.sh

--- builder/alpine/install.sh
+++ builder/alpine/install.sh
@@ -2,14 +2,14 @@
 COMMANDS=$1
 PACKAGES=
 # @todo why can I not make the IFS work here?!
+apk update
 while read COMMAND; do
-    if [ "$COMMAND" = "@*" ]; then
-      PACKAGE=${COMMAND:1}
-    else
-      PACKAGE=$(curl -sSfL "https://command-not-found.com/-/api/package/alpine/$COMMAND")
+    PACKAGE=$(curl -sSfL "https://command-not-found.com/-/api/package/alpine/$COMMAND")
+    if [ -z "${PACKAGE}" ] && apk info ${COMMAND} 1>&2 2>/dev/null; then
+      PACKAGE=${COMMAND}
     fi
-    
-    if apk info | grep -qE "^${PACKAGE}$" >/dev/null; then
+
+    if [ -n "${PACKAGE}" ] && apk info | grep -qE "^${PACKAGE}$" >/dev/null; then
         echo "Notice: Package already installed"
         exit 0
     fi
@@ -21,5 +21,6 @@ if [ -z "$PACKAGES" ]; then
     echo "Error: Nothing to install"
     exit 1
 fi
-apk --no-cache add ${PACKAGES}
+apk add ${PACKAGES}
+rm -rf /var/cache/apk/*
 exit 0
